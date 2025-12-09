e74a3f9a2b37986f1c822326e2edb2c45b1effc2
pdpagdatoon <patdavepagdatoon@gmail.com>
2025-12-09 17:11:38 -0500

    Admin Panel Comments

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import AdminComment
 
-# Register your models here.
+
+@admin.register(AdminComment)
+class AdminCommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at", "comment")
+    readonly_fields = ("user", "created_at", "comment")
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class AdminComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} – {self.created_at:%Y-%m-%d %H:%M}"
 
 
 class LogMessage(models.Model):
