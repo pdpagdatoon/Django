fa79fc42609c8c6c319745816642553097d326cf
Viswa <vp576@njit.edu>
2025-12-14 18:52:12 -0500

    Homepage pulls products from DB; preview support

--- hello/urls.py
+++ hello/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from hello import views
-from hello.views import ProductDetailView
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("products/<slug:slug>/", ProductDetailView.as_view(), name= "product-detail"), # to generate a url when adding a product through admin
+    path("product/<slug:slug>/", views.ProductDetailView.as_view(), name="product-detail"),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
-from django.views.generic import ListView
-from django.shortcuts import render
-from .utils import handle_comment_section
 from django.views.generic import DetailView
 from .models import Product
-        
-def home(request): # when you want to make a page with comments, copy this function but change the names
-    comment_form, comments = handle_comment_section(request)
-    return render(request, "hello/home.html", { 
-        "comment_form": comment_form,
-        "comments": comments,
-    }) 
+from .utils import handle_comment_section
+
+def home(request):
+    products = Product.objects.all().order_by("group", "title")
+    return render(request, "hello/home.html", {
+        "products": products,
+        "show_comments": False,
+    })
+
 
 # part of generating a page when adding a product through admin
 class ProductDetailView(DetailView):
     model = Product
     template_name = "hello/product_detail.html"
     context_object_name = "product"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        comment_form, comments = handle_comment_section(self.request)
+        context["comment_form"] = comment_form
+        context["comments"] = comments
+        context["show_comments"] = True
+        return context
+
